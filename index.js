ff04f70e78bee9f665876b2f85d5bf2df5e13087
Khanh Tran <katetrannbkhanh@gmail.com>
2022-11-08 16:49:44 -0800

    commit tree f653d12f39be63c4ce87ff13ba194ee0fdc5f30a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(emphatic="roller-skate"){
+    return `This Saturday, I want to ${emphatic}!`
+}
+
+function mondayWork(emphatic="go to the office"){
+    return `This Monday, I will ${emphatic}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function inner(emphatic='special'){
+        return `You are ${flair}${emphatic}${flair}!`
+    }
+}
+wrapAdjective('||')("a dedicated programmer");
